1fd4fe72c02390e7aac9ead4f6920a73fe0b22ea
Fadhel habeeb <fadhel@zilogic.com>
2019-08-06 15:32:23 +0530

    Added XmlBase class

--- include/xml.h
+++ include/xml.h
@@ -4,19 +4,24 @@
 #include <fstream>
 #include <string>
 
-class Xml2str {
+class XmlBase {
+ public:
+  enum {
+    DEFAULT,
+    FILE_OPEN_SUCCESS,
+    FILE_OPEN_FAILED,
+    FILE_EOF
+  };
+  int status = DEFAULT;
+};
+
+class Xml2str : public XmlBase {
 private:
   const std::string filename;
   std::string xml_str;
   std::ifstream infile;
   
 public:
-  enum {
-    FILE_OPEN_SUCCESS,
-    FILE_OPEN_FAILED,
-    FILE_EOF
-  };
-  int status;
 
   Xml2str(std::string file);
   std::string fetchString();
