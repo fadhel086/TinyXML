aaa25cdc7ee5b57549ae6eedac863f29bde37350
Fadhel habeeb <fadhel@zilogic.com>
2019-08-07 12:56:34 +0530

    Added XmlDeclaration, XmlAttribute class

--- include/xml.h
+++ include/xml.h
@@ -3,30 +3,85 @@
 
 #include <fstream>
 #include <string>
+#include <cstddef>
 
+/*
+ * class XmlBase
+ */
 class XmlBase {
  public:
-  enum {
-    DEFAULT,
-    FILE_OPEN_SUCCESS,
-    FILE_OPEN_FAILED,
-    FILE_EOF
+  enum OP_STATUS {
+    DEFAULT, FILE_OPEN_SUCCESS, FILE_OPEN_FAILED,
+    FILE_EOF, INVALID_XML_TYPE
   };
-  int status = DEFAULT;
-};
+  int opStatus = DEFAULT;
 
+  // May eliminate in future, as the present excercise may not require this.
+  enum XML_TYPE {
+    DOCUMENT, ELEMENT, DECLARATION,
+    NODE, COMMENT, UNKNOWN
+  };
+  int docType = UNKNOWN;
+  virtual std::string Parse(std::string& str) = 0;
+};
+// class XmlBase end
+// ********************************************************************************************
+
+/*
+ * class Xml2str
+ */
 class Xml2str : public XmlBase {
-private:
+ private:
   const std::string filename;
   std::string xml_str;
   std::ifstream infile;
-  
-public:
 
+ public:
   Xml2str(std::string file);
   std::string fetchString();
   std::string getString();
+  std::string Parse(std::string& str);
   ~Xml2str();
 };
+// class Xml2str end
+// *********************************************************************************************
 
+/*
+ * class XmlDeclaration
+ */
+class XmlDeclaration : public XmlBase {
+
+ private:
+  std::string version;
+  std::string encoding;
+  std::string standalone;
+  std::string decl_str;
+  bool declaration;
+
+ public:
+  XmlDeclaration();
+  std::string Parse(std::string& doc);
+  std::string getVersion();
+  std::string getEncoding();
+  std::string getStandalone();
+  std::string getDeclaration();
+};
+// class XmlDeclaration end
+// *********************************************************************************************
+
+/*
+ * class XmlAttribute
+ */
+class XmlAttribute : public XmlBase {
+
+ private:
+  std::string element;
+  
+ public:
+  XmlAttribute(std::string& element);
+  std::string Parse(std::string& attrib);
+
+};
+// class XmlAttribute
+// **********************************************************************************************
 #endif // XML_H
--- src/xml.cpp
+++ src/xml.cpp
@@ -3,32 +3,134 @@
 #include <string>
 #include "../include/xml.h"
 
+/*
+ * class Xml2str
+ */
 // Constructor, Opens file
-Xml2str::Xml2str(std::string file) : filename(file), infile(filename) {
+Xml2str::Xml2str(std::string file) : filename(file), infile(filename){
+  // Init variables
+  docType = XML_TYPE::DOCUMENT;
   if (!infile) {
     std::cerr << "Couldn't open the file" << std::endl;
-    status = FILE_OPEN_FAILED;
+    opStatus = OP_STATUS::FILE_OPEN_FAILED;
   } else {
-    status = FILE_OPEN_SUCCESS;
+    opStatus = OP_STATUS::FILE_OPEN_SUCCESS;
   }
 }
 
-// Get the string from xml_file
-std::string Xml2str::fetchString() {
-  if (status != FILE_OPEN_SUCCESS) {
-    return std::string();
+// Fetch the contents from xml_file to string
+std::string Xml2str::fetchString()
+{
+  if (opStatus != OP_STATUS::FILE_OPEN_SUCCESS) {
+    xml_str.clear();
+    return xml_str;
   }
 
   char c;
   while(infile.get(c)) {
     xml_str += c;
   }
-  status = FILE_EOF;
+  opStatus = OP_STATUS::FILE_EOF;
 
   return xml_str;
+}
+
+// Parse Implementation as it's a pure virtual function
+std::string Xml2str::Parse(std::string& str) {
+
+  // blank function, not used
+  return std::string();
+
 }
 
 // return the fetched string
 std::string Xml2str::getString() { return xml_str; }
 
 Xml2str::~Xml2str() { ; }
+// class Xml2str end
+// ****************************************************************************************
+
+/*
+ * class XmlDeclaration
+ */
+
+XmlDeclaration::XmlDeclaration()
+{
+  version.clear();
+  encoding.clear();
+  standalone.clear();
+  decl_str.clear();
+}
+std::string XmlDeclaration::getVersion() { return version; }
+std::string XmlDeclaration::getEncoding() { return encoding; }
+std::string XmlDeclaration::getStandalone() { return standalone; }
+std::string XmlDeclaration::getDeclaration() { return decl_str; }
+std::string XmlDeclaration::Parse(std::string& doc)
+{
+
+  if ((doc.find("<?xml") != std::string::npos) & (doc.find("?>") != std::string::npos)) {
+    // Declaration found
+    declaration = true;
+    decl_str = doc.substr(doc.find("<?xml"), (doc.find("?>") + 2));
+
+    // Get the version;
+
+    XmlAttribute attrib(decl_str);
+    std::string temp = "version=";
+    version = attrib.Parse(temp);
+    std::cout << version << std::endl;
+
+    if (decl_str.find("encoding")) {
+      temp = "encoding=";
+      encoding = attrib.Parse(temp);
+      std::cout << encoding << std::endl;
+    }
+
+    if (decl_str.find("standalone")) {
+      temp = "standalone=";
+      standalone = attrib.Parse(temp);
+      std::cout << standalone << std::endl;
+    }
+
+    return decl_str;
+    
+  } else {
+    declaration = false;
+    decl_str.clear();
+    return decl_str;
+  }    
+}
+// class XmlDeclaration
+// ****************************************************************************************
+
+/*
+ * class XmlAttribute
+ */
+
+XmlAttribute::XmlAttribute(std::string& element)
+{
+  /*
+   * Declaration should be of below form:
+   * <?xml version="1.0" ?> OR
+   * <?xml version="1.0" encoding="UTF-8" standalone="yes|no" ?>
+   */
+  this->element = element;
+}
+std::string XmlAttribute::Parse(std::string& attrib)
+{
+  //
+  std::string temp;
+  char delim;
+  size_t pos = element.find(attrib) + attrib.length();
+  delim = element[pos]; // attribute could be between single or double quotes
+  
+  // now pos will point to start of attribute
+  ++pos;
+  while (element[pos] != delim) {
+    temp += element[pos++];
+  }
+  return temp;;
+}
+
+// class XmlAttribute
+// ******************************************************************************************
